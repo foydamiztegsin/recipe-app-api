c0d0b6feeecad56a4ae0055c6c8501d645ba399d
BunyodIsmailov <ishonch055@mail.ru>
2023-04-09 17:49:22 +0500

    Normalize commit

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -21,11 +21,11 @@ class ModelTest(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
-        """Test email is normalized for new user."""
+        """Test email is normalized for new users."""
         sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
-            ['Test2@Example.com', 'test2@example.com'],
-            ['TEST3@EXAMPLE.COM', 'test3@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
             ['test4@example.COM', 'test4@example.com'],
         ]
         for email, expected in sample_emails:
