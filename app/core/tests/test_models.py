1bd0adab9da1707106d764339a6e7656fc4957a2
BunyodIsmailov <ishonch055@mail.ru>
2023-04-09 15:21:19 +0500

    Normalize email

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,32 @@
+"""
+Test fo models.
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTest(TestCase):
+    """Test models."""
+
+    def test_create_user_with_email_successful(self):
+        """Test creating  user with an email is successfull."""
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new user."""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'test3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
