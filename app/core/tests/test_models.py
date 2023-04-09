c354ffdbcb5e818d62f1123ba7291e2529cf8c25
BunyodIsmailov <ishonch055@mail.ru>
2023-04-09 18:54:31 +0500

    Django Admin Page

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -35,8 +35,7 @@ class ModelTest(TestCase):
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('', 'test123')             
-             
+            get_user_model().objects.create_user('', 'test123')          
     def test_create_superuser(self):
         """Test creating a superuser."""
         user = get_user_model().objects.create_superuser(
