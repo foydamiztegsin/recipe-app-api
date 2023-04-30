1fa802a0c5f37323e85e0842b105128fb5c0d1fb
BunyodIsmailov <ishonch055@mail.ru>
2023-04-30 18:47:32 +0500

    ports

--- app/app/settings.py
+++ app/app/settings.py
@@ -87,6 +87,7 @@ DATABASES = {
         'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASS"),
+        'PORT': os.environ.get("DB_PORT"),
         
     }
 }
