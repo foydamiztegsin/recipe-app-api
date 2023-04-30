7d2c3599ac4e77e96044c341d743efd7b7159475
BunyodIsmailov <ishonch055@mail.ru>
2023-04-30 12:23:21 +0500

    depl

--- app/core/models.py
+++ app/core/models.py
@@ -19,7 +19,7 @@ class UserManager(BaseUserManager):
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-
+        
         return user
     
     def create_superuser(self, email, password):
